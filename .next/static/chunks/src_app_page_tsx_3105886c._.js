3b8a9918b2aef5c2314225301124030369a25b57
Oráculo IA <sistemaoraculoia@gmail.com>
2025-07-28 17:46:17 +0000

    feat: Sincroniza estado do projeto com o repositório - Inclui todas as últimas correções de bugs e melhorias de funcionalidade. - Garante que a base de código local esteja atualizada e pronta para novos desenvolvimentos.

--- .next/static/chunks/src_app_page_tsx_3105886c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_cc06ae20._.js",
-  "static/chunks/src_components_landing-page_example-pie-chart_tsx_57648335._.js"
-],
-    source: "dynamic"
-});
